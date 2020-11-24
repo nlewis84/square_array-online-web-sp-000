e85baa4d48d7c1e1a78dfbda1644acac170c36a2
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-24 04:55:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
 require "pry"
 
-def square_array(array)
-  count = 0
-  array.each do |index|
-    array[count] = index**2
-    count += 1
-  end
-end
+# def square_array(array)
+#   count = 0
+#   array.each do |index|
+#     array[count] = index**2
+#     count += 1
+#   end
+# end
 
 def square_array(array)
   collect array : #{array.collect {|x| x**2 }}
